09769c7f37304713d489dfd22832d9ccd1c8c80c
taylor124 <74930599+taylor124@users.noreply.github.com>
2021-03-01 12:42:43 -0500

    Added Generate HTML

--- src/page-template.js
+++ src/page-template.js
@@ -1,6 +1,8 @@
-
+module.exports = (name, github) => {
+    return `
     <!DOCTYPE html> 
     <html lang="en"> 
+    
     <head>
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,8 +11,9 @@
     </head>
   
     <body>
-      <h1>Taylor</h1>
-      <h2><a href="https://github.com/taylor124">Github</a></h2>
+      <h1>${name}</h1>
+      <h2><a href="https://github.com/${github}">Github</a></h2>
     </body>
     </html>
-    
+    `;
+  };
